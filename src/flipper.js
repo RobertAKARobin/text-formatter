e949c0946fd6946ad4dc842270e6e55b7ce32ef5
RobertAKARobin <robertgfthomas@gmail.com>
2018-06-15 09:05:19 -0500

    Made block test pass

--- src/flipper.js
+++ src/flipper.js
@@ -15,8 +15,23 @@ const inlineMapping = [
 	}
 })
 
+const blockMapping = [
+	['-', 'li']
+].map(mapping=>{
+	let [c, tag] = mapping
+	return {
+		tester: new RegExp(`(^|\\n|\\r)\\${c}\\s*(.*)(?=$|\\n|\\r)?`, 'g'),
+		replacer: function(nil, newline, match){
+			return `${newline}<${tag}>${match}</${tag}>`
+		}
+	}
+})
+
 module.exports = function flipped(input){
 	inlineMapping.forEach(mapping=>{
+		input = input.replace(mapping.tester, mapping.replacer)
+	})
+	blockMapping.forEach(mapping=>{
 		input = input.replace(mapping.tester, mapping.replacer)
 	})
 	return input
