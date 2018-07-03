6d906a504fe4135cf6966dbf76e72d3c2d307ce2
RobertAKARobin <robertgfthomas@gmail.com>
2018-07-03 16:28:40 -0500

    Added additional specs for blocks

--- spec/block-spec.js
+++ spec/block-spec.js
@@ -9,5 +9,23 @@ function testBlockElement(c, tag){
 		shouldDo(`\n${c} when it begins a newline`)
 			.as( `\n<${tag}>when it begins a newline</${tag}>`)
 
+		shouldDo(`\n\n${c} when it begins a newline many times\n`)
+			.as( `\n\n<${tag}>when it begins a newline many times</${tag}>\n`)
+		
+		shouldDo(`${c}when it is the first line`)
+			.as( `<${tag}>when it is the first line</${tag}>`)
+
+		shouldDo(`\n  	${c}when it is preceded by whitespace`)
+			.as( `\n  	<${tag}>when it is preceded by whitespace</${tag}>`)
+
+		shouldDo(`\n0${c}but not when preceded by a number`)
+			.as( `\n0${c}but not when preceded by a number`)
+		
+		shouldDo(`\n+${c}but not when preceded by a special character`)
+			.as( `\n+${c}but not when preceded by a special character`)
+		
+		shouldDo(`\n${c}${c}only once if the character is included many times`)
+			.as( `\n<${tag}>${c}only once if the character is included many times</${tag}>`)
+
 	})
 }
