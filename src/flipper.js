27d5322088299a626f33a3a8e2c894b65cbb89ae
RobertAKARobin <robertgfthomas@gmail.com>
2018-07-17 10:53:40 -0400

    DRYed up flipper method

--- src/flipper.js
+++ src/flipper.js
@@ -1,38 +1,56 @@
-const inlineMapping = [
-	['*', 'b'],
-	['_', 'u'],
-	['/', 'i'],
-	['~', 's'],
-	['=', 'mark'],
-	['`', 'code']
-].map(mapping=>{
-	let [c, tag] = mapping
-	return {
-		tester: new RegExp(`\\${c}{2}([^\n\r]*?)\\${c}{2}`, 'g'),
-		replacer: function(nil, match){
-			return `<${tag}>${match}</${tag}>`
-		}
-	}
-})
+const elementTypes = [
+  {
+    name: 'inline',
+    generateTester: (c)=>{
+      return new RegExp(`\\${c}{2}([^\n\r]*?)\\${c}{2}`, 'g')
+    },
+    generateReplacer: (tag)=>{
+      return function(nil, match){
+        return `<${tag}>${match}</${tag}>`
+      }
+    },
+    mappingPairs: [
+      ['*', 'b'],
+      ['_', 'u'],
+      ['/', 'i'],
+      ['~', 's'],
+      ['=', 'mark'],
+      ['`', 'code']
+    ]
+  },
+  {
+    name: 'block',
+    generateTester: (c)=>{
+      return new RegExp(`(^|\\n|\\r)\\${c}\\s*(.*)(?=$|\\n|\\r)?`, 'g')
+    },
+    generateReplacer: (tag)=>{
+      return function(nil, newline, match){
+        return `${newline}<${tag}>${match}</${tag}>`
+      }
+    },
+    mappingPairs: [
+      ['-', 'li']
+    ]
+  }
+]
 
-const blockMapping = [
-	['-', 'li']
-].map(mapping=>{
-	let [c, tag] = mapping
-	return {
-		tester: new RegExp(`(^|\\n|\\r)\\${c}\\s*(.*)(?=$|\\n|\\r)?`, 'g'),
-		replacer: function(nil, newline, match){
-			return `${newline}<${tag}>${match}</${tag}>`
-		}
-	}
+const gauntlet = []
+elementTypes.forEach((elementType)=>{
+  elementType.mappingPairs.forEach((mappingPair)=>{
+    const inputCharacter = mappingPair[0]
+    const outputTag = mappingPair[1]
+    const tester = elementType.generateTester(inputCharacter)
+    const replacer = elementType.generateReplacer(outputTag)
+    gauntlet.push([
+      tester,
+      replacer
+    ])
+  })
 })
 
 module.exports = function flipped(input){
-	inlineMapping.forEach(mapping=>{
-		input = input.replace(mapping.tester, mapping.replacer)
-	})
-	blockMapping.forEach(mapping=>{
-		input = input.replace(mapping.tester, mapping.replacer)
-	})
+  gauntlet.forEach(mapping=>{
+    input = input.replace(mapping[0], mapping[1])
+  })
 	return input
 }
