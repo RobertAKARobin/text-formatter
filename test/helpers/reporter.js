bbf39f021e326eea99ee5f8a35e77237dedad27c
RobertAKARobin <robertgfthomas@gmail.com>
2018-07-27 16:21:06 +0000

    Added original input to tests

--- test/helpers/reporter.js
+++ test/helpers/reporter.js
@@ -1,5 +1,18 @@
 const mocha = require('mocha')
 
+function color(string){
+	const bgYel = "\x1b[43m"
+	const txBlk = "\x1b[30m"
+	const reset = "\x1b[0m"
+	return `${bgYel}${txBlk}${string}${reset}` 
+}
+
+function swapWhitespace(input){
+	return input
+		.replace(/\t/g, color('\\t'))
+		.replace(/[\n\r]/g, color('\\n'))
+}
+
 module.exports = function(runner){
 	mocha.reporters.Base.call(this, runner)
 	let passes = 0, fails = 0
@@ -11,8 +24,9 @@ module.exports = function(runner){
 	runner.on('fail', (test, err)=>{
 		fails += 1
 		console.log(`\n===FAIL ${fails}===`)
-		console.log('Exp: ' + err.expected)
-		console.log('Act: ' + err.actual)
+		console.log('Inp: ' + swapWhitespace(test.title))
+		console.log('Exp: ' + swapWhitespace(err.expected))
+		console.log('Act: ' + swapWhitespace(err.actual))
 	})
 	
 	runner.on('end', ()=>{
--- test/helpers/should.js
+++ test/helpers/should.js
@@ -3,25 +3,12 @@ const assert = require('assert')
 
 console.log(flipped.printedRegex)
 
-function color(string){
-	const bgYel = "\x1b[43m"
-	const txBlk = "\x1b[30m"
-	const reset = "\x1b[0m"
-	return `${bgYel}${txBlk}${string}${reset}` 
-}
-
-function swapWhitespace(input){
-	return input
-		.replace(/\t/g, color('\\t'))
-		.replace(/[\n\r]/g, color('\\n'))
-}
-
 module.exports = function shouldDo(shouldStatement){
 	return {
 		as: (result)=>{
 			it(shouldStatement, ()=>{
 				let test = flipped(shouldStatement)
-				assert.equal(swapWhitespace(test), swapWhitespace(result))
+				assert.equal(test, result)
 			})
 		}
 	}
