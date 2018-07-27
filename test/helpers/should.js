2f221d76706dd71d7c889172dbd20f34fb6decec
RobertAKARobin <robertgfthomas@gmail.com>
2018-07-27 16:15:24 +0000

    Color whitespace

--- test/helpers/should.js
+++ test/helpers/should.js
@@ -3,10 +3,17 @@ const assert = require('assert')
 
 console.log(flipped.printedRegex)
 
+function color(string){
+	const bgYel = "\x1b[43m"
+	const txBlk = "\x1b[30m"
+	const reset = "\x1b[0m"
+	return `${bgYel}${txBlk}${string}${reset}` 
+}
+
 function swapWhitespace(input){
 	return input
-		.replace(/\t/g, '\\T')
-		.replace(/[\n\r]/g, '\\N')
+		.replace(/\t/g, color('\\t'))
+		.replace(/[\n\r]/g, color('\\n'))
 }
 
 module.exports = function shouldDo(shouldStatement){
