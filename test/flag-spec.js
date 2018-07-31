ccc6af87fff9b935fb03cb64fa2bda342a959d25
RobertAKARobin <robertgfthomas@gmail.com>
2018-07-31 18:57:55 +0000

    Removed some tests for flags; KISS

--- test/flag-spec.js
+++ test/flag-spec.js
@@ -12,32 +12,20 @@ const flagTests = [
 		shouldDo(`[${c} when it begins the string/]`)
 			.as( `<${tag}> when it begins the string</${tag}>`)
 		
-		shouldDo(`Not when preceded[${c}by other characters on its line/]`)
-			.asUnchanged()
-		
 		shouldDo(`\t [${c} when preceded by whitespace/]`)
 			.as( `\t <${tag}> when preceded by whitespace</${tag}>`)
 		
 		shouldDo(`\t [${c}\t and preserve whitespace\t /]`)
 			.as( `\t <${tag}>\t and preserve whitespace\t </${tag}>`)
 		
-		shouldDo(`[${c} not when not ended by close tag`)
-			.asUnchanged()
-		
 		shouldDo(`[${c} when spanning \n\n\nmany lines/]`)
 			.as( `<${tag}> when spanning \n\n\nmany lines</${tag}>`)
-		
-		shouldDo(`[${c} when close tag comes/]in middle of string, inserting newline`)
-			.as( `<${tag}> when close tag comes</${tag}>\nin middle of string`)
 			
 		shouldDo(`[${c} when nested\n[${c}and preceded by newline/]/]`)
-			.as( `<${tag}> when nested\n<${tag}>and preceded by newline</${tag}>\n</${tag}>`)
-		
-		shouldDo(`[${c} not when nested[${c} and preceded by newline/]/]`)
-			.as( `<${tag}> not when nested${c} and preceded by newline</${tag}>\n/]`)
+			.as( `<${tag}> when nested\n<${tag}>and preceded by newline</${tag}></${tag}>`)
 		
 		shouldDo(`[${c} and close tag should match\n[${c} first open tag/]like so`)
-			.as( `<${tag}> and close tag should match\n[${c} first open tag</${tag}\nlike so`)
+			.as( `<${tag}> and close tag should match\n[${c} first open tag</${tag}like so`)
 		
 	})
 })
