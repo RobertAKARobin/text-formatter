0d09b8a81db51749fbf534919d238228d4524a92
RobertAKARobin <robertgfthomas@gmail.com>
2018-07-31 18:36:31 +0000

    Made 'fail' headers red

--- test/helpers/reporter.js
+++ test/helpers/reporter.js
@@ -1,16 +1,16 @@
 const mocha = require('mocha')
 
-function color(string){
-	const bgYel = "\x1b[43m"
-	const txBlk = "\x1b[30m"
-	const reset = "\x1b[0m"
-	return `${bgYel}${txBlk}${string}${reset}` 
-}
+const bgYel = "\x1b[43m"
+const bgRed = "\x1b[41m"
+const txBlk = "\x1b[30m"
+const txRed = "\x1b[31m"
+const txWht = "\x1b[37m"
+const reset = "\x1b[0m"
 
 function swapWhitespace(input){
 	return input
-		.replace(/\t/g, color('\\t'))
-		.replace(/[\n\r]/g, color('\\n'))
+		.replace(/\t/g, `${bgYel}${txBlk}\\t${reset}`)
+		.replace(/[\n\r]/g, `${bgYel}${txBlk}\\n${reset}`)
 }
 
 module.exports = function(runner){
@@ -23,7 +23,7 @@ module.exports = function(runner){
 	
 	runner.on('fail', (test, err)=>{
 		fails += 1
-		console.log(`\n===FAIL ${fails}===`)
+		console.log(`\n${bgRed}${txWht}===FAIL ${fails}===${reset}`)
 		console.log('Inp: ' + swapWhitespace(test.title))
 		console.log('Exp: ' + swapWhitespace(err.expected))
 		console.log('Act: ' + swapWhitespace(err.actual))
